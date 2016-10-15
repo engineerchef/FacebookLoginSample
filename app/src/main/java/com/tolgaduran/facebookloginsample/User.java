4715c389f82f610fc1ce2d0bbf182d36f9566cb9
Tolga DURAN <tolgaduran1980@gmail.com>
2016-10-16 01:00:42 +0300

    Initial commit

---
+++ app/src/main/java/com/tolgaduran/facebookloginsample/User.java
@@ -0,0 +1,12 @@
+package com.tolgaduran.facebookloginsample;
+
+/**
+ * Created by Java_Engineer on 07.10.2016.
+ */
+
+public class User {
+    public String name;
+    public String email;
+    public String facebookID;
+    public String gender;
+}
